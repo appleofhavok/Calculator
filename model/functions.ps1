18f432bf8ecba7248a9c4d5fafb0284aca5694c4
Apolonia <adogoli@wgu.edu>
2023-02-15 10:06:50 -0500

    Added displayanswer function using concat and interpolation

--- model/functions.ps1
+++ model/functions.ps1
@@ -1,9 +1,29 @@
 #If 3-2=1 it works
-function Subtract {
-    param ( 
+#Good luck
+#Parameters work like this:
+#Function Declaration: - Function functionName(){}
+#Function Parameters: Function functionName(paramater1, parameter2, parameer3){}
+#Function Body w/ Parameters: Function functionName(parameter1){ print(paramemter1) }
+
+# function totalILvL {
+#     param (
+#         [int]$head,
+#         [int]$shoulders,
+#         [int]$boots,
+#         [int]$weapon
+#     )
+#     [int]$ilvl = ($head+$boots+$shoulders+$weapon)/4
+# }
+
+#subtract(1,3);
+#subtract(a,b);
+
+function Subtract { 
+    param (
         [int]$a,
         [int]$b
-    )
+    ) 
+    return $a-$b
 }
 
 
@@ -13,6 +33,7 @@ function Add {
         [int]$a,
         [int]$b
     )
+    return $a+$b
 }
 
 #If 3*3=9 it works
@@ -41,4 +62,22 @@ function Remainder{
     )
 }
 
-Write-Host "Hello world"
+function DisplayAnswer {
+    param(
+        [int]$answer
+    ) 
+    $string1 = "The answer is"
+    $string2 = $answer
+    $string1 + " " + $string2
+
+    $a = 1
+    $b = 2
+    $c = 3
+    "I can count $($a), $($b), $($c)"
+    #Print "I can count: 1, 2, 3" using string interpolation
+}
+
+
+DisplayAnswer(Add 4 3)
+DisplayAnswer(Subtract 4 3)
+#TO DO add functionality and print result for each function
